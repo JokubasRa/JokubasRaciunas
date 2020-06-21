40cbbe0244a12fd11ac6749a39b5d288e2a500c5
JokubasRa <67127123+JokubasRa@users.noreply.github.com>
2020-06-21 22:30:42 +0300

    Initial commit

---
+++ JokubasRaciunas/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JokubasRaciunas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
